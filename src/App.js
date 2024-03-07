96349441397db790d58ec0a004b75ab672d21a66
tamjidzihan <tamjidzihan@gmail.com>
2024-03-07 14:18:13 +0600

    home 2024-03-07

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 
-function Header() {
-  return <h1>Hello World</h1>;
-}
+// function Header() {
+//   return <h1>Hello World</h1>;
+// }
 
 
-function App() {
-
-  return <Header />;
-}
+function App(props) {
+  return (
+    <h1>{props.title}</h1>
+  );
+};
 
 export default App;
