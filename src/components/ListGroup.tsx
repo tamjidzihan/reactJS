d8447095ecec4971afef0c358cc6b95b9b4ea839
tamjidzihan <tamjidzihan@gmail.com>
2024-03-13 19:33:33 +0600

    2024-03-13

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { MouseEvent, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 
 
@@ -12,10 +12,13 @@ function ListGroup() {
         'Barishal',
         'Rangpur'
     ];
+    // items = [];
 
-    items = [];
+    const [selecetedIndex, setSelecetedIndex] = useState(-1)
+    console.log(selecetedIndex);
+    // console.log(setSelecetedIndex);
 
-
+    // const handleClick = (e: MouseEvent) => console.log(e);
 
     return (
         <>
@@ -23,18 +26,27 @@ function ListGroup() {
                 <h1 className=' fw-light text-center '>List</h1>
                 {items.length === 0 && <p className=' display-6 text-center  '>No item Found</p>}
                 <ul className="list-group">
-
                     {
-                        items.map(items =>
-                            <li key={items} className="list-group-item">{items}</li>
-                        )}
+                        items.map((item, index) =>
+                            <li
+                                className={selecetedIndex === index
+                                    ? "list-group-item text-start active"
+                                    : "list-group-item text-start "} role='button'
+                                key={item}
+                                // onClick={() => console.log(index, item)}
+                                onClick={() => setSelecetedIndex(index)}
+                            >
+                                {index + 1}: {item}
+                            </li>
+                        )
+                    }
                     {/* <li className="list-group-item">An item</li>
                     <li className="list-group-item">A second item</li>
                     <li className="list-group-item">A third item</li>
                     <li className="list-group-item">A fourth item</li>
                     <li className="list-group-item">And a fifth one</li> */}
                 </ul>
-            </div>
+            </div >
         </>
     );
 }
