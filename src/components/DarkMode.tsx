80d143f80be90e6f046b029ff91a6bc5fd5e57c2
tamjidzihan <tamjidzihan@gmail.com>
2024-03-18 14:33:23 +0600

    2024-03-18

---
+++ src/components/DarkMode.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState } from 'react';
+
+function DarkMode() {
+    const darkMode = 'Dark Mode Seleceted'
+    const lightMode = 'Lighe Mode Seleceted'
+
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default DarkMode;
