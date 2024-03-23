66bb5614f76380744321e06797d453baab81701f
tamjidzihan <tamjidzihan@gmail.com>
2024-03-23 21:25:59 +0600

    office 2024-03-23

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -12,7 +12,7 @@ function ListGroup() {
         'Barishal',
         'Rangpur'
     ];
-    // items = [];
+    items = [];
 
     const [selecetedIndex, setSelecetedIndex] = useState(-1)
     console.log(selecetedIndex);
@@ -20,26 +20,27 @@ function ListGroup() {
 
     // const handleClick = (e: MouseEvent) => console.log(e);
 
+
+    const listItems = items.map((item, index) =>
+        <li
+            className={selecetedIndex === index
+                ? "list-group-item text-start active"
+                : "list-group-item text-start "} role='button'
+            key={item}
+            // onClick={() => console.log(index, item)}
+            onClick={() => setSelecetedIndex(index)}
+        >
+            {index + 1}: {item}
+        </li>
+    )
+
+
     return (
         <>
-            <div className='container my-2'>
-                <h1 className=' fw-light text-center '>List</h1>
+            <div className='container my-2 fadeUpDown'>
+                <h1 className=' rounded-5  fw-bold text-center text-white  text-uppercase bg-primary '>Districts of bangladesh</h1>
                 {items.length === 0 && <p className=' display-6 text-center  '>No item Found</p>}
-                <ul className="list-group">
-                    {
-                        items.map((item, index) =>
-                            <li
-                                className={selecetedIndex === index
-                                    ? "list-group-item text-start active"
-                                    : "list-group-item text-start "} role='button'
-                                key={item}
-                                // onClick={() => console.log(index, item)}
-                                onClick={() => setSelecetedIndex(index)}
-                            >
-                                {index + 1}: {item}
-                            </li>
-                        )
-                    }
+                <ul className="list-group">{listItems}
                     {/* <li className="list-group-item">An item</li>
                     <li className="list-group-item">A second item</li>
                     <li className="list-group-item">A third item</li>
