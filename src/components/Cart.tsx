baa40a753b71342a2ba3b1cae6ca378f277f5742
tamjidzihan <tamjidzihan@gmail.com>
2024-04-30 02:01:11 +0600

    home 2024-04-30

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -11,7 +11,8 @@ function Cart({ cartItems, onClear }: Props) {
 
             <div className="container">
                 <h2>Cart Items:</h2>
-                {cartItems.length === 0 && <p>No cart Item found</p>}
+                {cartItems.length === 0 && <p>No cart Item found
+                    <FontAwesomeIcon icon="fa-solid fa-delete-left" /></p>}
                 <ul className="list-group">
                     {cartItems.map(item =>
                         <li className="list-group-item" key={item}>{item}</li>
